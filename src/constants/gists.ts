cdacaa7a75fe8b6eb9b04fd4198d2dead9d0bad7
Alexey Yakovlev <alexeyyakovlev0359@gmail.com>
2021-08-14 21:11:45 +0300

    feat: add puppeteer config example

--- src/constants/gists.ts
+++ src/constants/gists.ts
@@ -13,8 +13,14 @@ const HERMIONE_CONFIG_GIST_ID = 'f00bd24f2a757508ca3955100fe973ec';
  */
 const PLAYWRIGHT_CONFIG_GIST_ID = '036ee25b0176817cdbab1b2e573a78e4';
 
+/**
+ * Gist containing required files for `puppeteer` template
+ */
+const PUPPETEER_CONFIG_GIST_ID = 'e83c4d3132b978d76ec9da8990035c4d';
+
 export {
     DEFAULT_CONFIG_GIST_ID,
     HERMIONE_CONFIG_GIST_ID,
     PLAYWRIGHT_CONFIG_GIST_ID,
+    PUPPETEER_CONFIG_GIST_ID,
 };
